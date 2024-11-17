98afd3c350a8280bee7871894e2153393c1e777f
Rishav176 <rishavkr1766@gmail.com>
2024-11-16 20:03:16 -0700

    Updated UI and zoom target pos

--- components/RenderModel.jsx
+++ components/RenderModel.jsx
@@ -15,16 +15,19 @@ const RenderModel = forwardRef((props, ref) => {
   const cameraRef = useRef();
   const [isAnimating, setIsAnimating] = useState(false);
 
-  const initialPosition = { x: -17, y:3, z: -25}; // Original starting position
-  const initialLookAt = { x:0, y: 0, z: 0 }; // Original look-at target
+  const initialPosition = { x: -17, y: 3, z: -25 }; // Original starting position
+  const initialLookAt = { x: 0, y: 0, z: 0 }; // Original look-at target
 
   // Function to zoom to the SnowHut
   const handleZoomToSnowHut = () => {
     if (isAnimating || !cameraRef.current) return;
 
     setIsAnimating(true);
-
-    const targetPosition = { x: -1, y: 0, z: 5 };
+    const controls = cameraRef.current.parent.children.find(
+      (child) => child.type === "OrbitControls"
+    );
+    if (controls) controls.enabled = false;
+    const targetPosition = { x: -3, y: 0, z: -5 };
     const lookAtPosition = { x: 0, y: 0, z: 0 };
 
     gsap.to(cameraRef.current.position, {
@@ -42,14 +45,12 @@ const RenderModel = forwardRef((props, ref) => {
       },
       onComplete: () => {
         setIsAnimating(false);
+        if (controls) controls.enabled = true;
       },
     });
   };
 
-
-  const handelZoomToGarden = () => {
-    
-  }
+  const handelZoomToGarden = () => {};
 
   // Function to reset the camera to the original starting position
   const handleResetView = () => {
@@ -95,7 +96,12 @@ const RenderModel = forwardRef((props, ref) => {
             position={[initialPosition.x, initialPosition.y, initialPosition.z]}
             ref={cameraRef}
           />
-          <OrbitControls enabled={!isAnimating} />
+          <OrbitControls
+            enabled={!isAnimating}
+            maxPolarAngle={Math.PI / 2}
+            maxDistance={33}
+            minDistance={2}
+          />
           <ambientLight intensity={0.5} />
           <SnowHut />
         </Suspense>
--- components/index.jsx
+++ components/index.jsx
@@ -1,16 +1,20 @@
 "use client";
 import React from "react";
-import { Activity, HeartPulse, Wind, Music, LoaderPinwheel } from "lucide-react";
+import {
+  Activity,
+  HeartPulse,
+  Wind,
+  Music,
+  LoaderPinwheel,
+} from "lucide-react";
 
-
-const Navigation = ({ onIconClick ,onReset,showGoBack = false}) => {
+const Navigation = ({ onIconClick, onReset, showGoBack = false }) => {
   const buttonList = [
-
     {
       label: "Music",
       icon: "music",
       component: Music,
-    }
+    },
     //{
     //  //label: "Meditation",
     //  //icon: "activity",
@@ -40,44 +44,37 @@ const Navigation = ({ onIconClick ,onReset,showGoBack = false}) => {
 
   return (
     <div className="absolute bottom-5 w-full flex justify-center gap-x-12 z-50">
-        {buttonList.map((button, index) => (
-            <div key={index} className="relative group/item">
-                {/* Tooltip */}
-                <div className="pointer-events-none absolute -top-8 left-1/2 transform -translate-x-1/2 -translate-y-1 bg-black/40 text-white text-sm px-3 py-1 rounded-md opacity-0 group-hover/item:opacity-100 group-hover/item:translate-y-0 transition-all duration-300 ease-in-out">
-                    {button.label}
-                </div>
-                {/* Main Button */}
-                <button
-                    onClick={() => onIconClick(button.icon)}
-                    className="relative w-40 h-20 flex items-center justify-center before:content-[''] 
-                    before:absolute before:w-full before:h-full before:bg-white/10 before:[clip-path:polygon(25%_0%,75%_0%,100%_50%,75%_100%,25%_100%,0%_50%)] 
-                    before:border before:border-white/0 before:hover:bg-white/20 before:transition-all before:duration-300 hover:before:scale-105 active:before:bg-white/30"
-                    //className=""
-                    aria-label={button.label}
-                >
-                    <button.component
-                        size={48}
-                        className="text-white"
-                    />
-                </button>
-            </div>
-        ))}
+      {buttonList.map((button, index) => (
+        <div key={index} className="relative group/item">
+          {/* Tooltip */}
+          <div className="pointer-events-none absolute -top-8 left-1/2 transform -translate-x-1/2 -translate-y-1 bg-black/40 text-white text-sm px-3 py-1 rounded-md opacity-0 group-hover/item:opacity-100 group-hover/item:translate-y-0 transition-all duration-300 ease-in-out">
+            {button.label}
+          </div>
+          {/* Main Button */}
+          <button
+            onClick={() => onIconClick(button.icon)}
+            className="rounded-xl w-24 bg-slate-300 px-3.5 py-2.5 text-sm font-semibold text-dark shadow-lg hover:bg-slate-500 hover:scale-125 transition-transform duration-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-700"
+            //className=""
+            aria-label={button.label}
+          >
+            Start
+          </button>
+        </div>
+      ))}
 
-        {/* Go Back Button */}
-        {showGoBack && (
-            <div className="relative group/item">
-                <button
-                    onClick={onReset}
-                    className="relative w-40 h-20 flex items-center justify-center before:content-[''] 
-                    before:absolute before:w-full before:h-full before:bg-white/10 before:[clip-path:polygon(25%_0%,75%_0%,100%_50%,75%_100%,25%_100%,0%_50%)] 
-                    before:border before:border-white/0 before:hover:bg-white/20 before:transition-all before:duration-300 hover:before:scale-105 active:before:bg-white/30"
-                >
-                    Go Back
-                </button>
-            </div>
-        )}
+      {/* Go Back Button */}
+      {showGoBack && (
+        <div className="relative group/item">
+          <button
+            onClick={onReset}
+            className="rounded-xl bg-slate-300 w-24 px-3.5 py-2.5 text-sm font-semibold text-dark shadow-sm hover:bg-slate-500 hover:scale-125 transition-transform duration-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-700"
+          >
+            Go Back
+          </button>
+        </div>
+      )}
     </div>
-);
+  );
 };
 
 export default Navigation;
